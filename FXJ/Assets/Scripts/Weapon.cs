1cf58c9cbb8424661f852abbf117a9fb6166b403
Elias Herman <elias.herman01@gmail.com>
2022-02-03 16:25:01 +0100

    Crosshair + broke some shit

---
+++ FXJ/Assets/Scripts/Weapon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : NetworkedBehaviour
+{
+
+    public void FireInput(bool newFireState)
+    {
+        Debug.Log("inshallah");
+    }
+
+}
